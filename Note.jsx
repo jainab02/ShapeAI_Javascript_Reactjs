8180a9151eeae43aca18e756e20852d576745e36
zainab02-boop <84964912+zainab02-boop@users.noreply.github.com>
2021-06-06 18:09:48 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Note() {
+  return (
+    <div>
+      <h1>Javascript and React.js</h1>
+
+      <p> This Was an Amazing Bootcamp taken up by shaurya sinha sir . he covered almost 
+        all the things like javascript , html , react js, etc .
+
+      </p>
+      </div>
+  );
+}
+export default Note;
